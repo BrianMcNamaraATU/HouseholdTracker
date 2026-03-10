f3e592205fb44e83ce81d2fcb0315768a1a02117
Brian McNamara <L00196743@atu.ie>
2026-03-10 11:19:33 +0000

    RegisteredUser Model and Tests Closes #1

---
+++ HouseholdTracker.Core/Models/RegisteredUser.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+
+namespace HouseholdTracker.Core.Models;
+
+internal class RegisteredUser
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Username { get; internal set; } = string.Empty;
+    public string Password { get; internal set; } = string.Empty;
+    public string FirstName { get; internal set; } = string.Empty;
+    public string LastName { get; internal set; } = string.Empty;
+    public string Email { get; internal set; } = string.Empty;
+    public string APIKey { get; internal set; } = string.Empty;
+
+    public RegisteredUser() { }
+
+    public RegisteredUser(int id, string username, string password, string firstName, string lastName, string email, string aPIKey)
+    {
+        Id = id;
+        Username = username;
+        Password = password;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        APIKey = aPIKey;
+    }
+}
---
+++ HouseholdTracker.Tests/Models/RegisteredUserTests.cs
@@ -0,0 +1,41 @@
+﻿using HouseholdTracker.Core.Models;
+
+namespace HouseholdTrackerTests;
+
+[TestFixture]
+internal sealed class RegisteredUserTests
+{
+    [Test]
+    public void DefaultConstructorTest()
+    {
+        var registeredUser = new RegisteredUser();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(registeredUser.Id, Is.Zero);
+            Assert.That(registeredUser.Username, Is.EqualTo(string.Empty));
+            Assert.That(registeredUser.Password, Is.EqualTo(string.Empty));
+            Assert.That(registeredUser.FirstName, Is.EqualTo(string.Empty));
+            Assert.That(registeredUser.LastName, Is.EqualTo(string.Empty));
+            Assert.That(registeredUser.Email, Is.EqualTo(string.Empty));
+            Assert.That(registeredUser.APIKey, Is.EqualTo(string.Empty));
+        }
+    }
+
+    [Test]
+    public void DefaultSetterTest()
+    {
+        var registeredUser = new RegisteredUser(1, "brian", "mcnamara", "Brian", "McNamara", "brian@email.com", "123456acdef");
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(registeredUser.Id, Is.EqualTo(1));
+            Assert.That(registeredUser.Username, Is.EqualTo("brian"));
+            Assert.That(registeredUser.Password, Is.EqualTo("mcnamara"));
+            Assert.That(registeredUser.FirstName, Is.EqualTo("Brian"));
+            Assert.That(registeredUser.LastName, Is.EqualTo("McNamara"));
+            Assert.That(registeredUser.Email, Is.EqualTo("brian@email.com"));
+            Assert.That(registeredUser.APIKey, Is.EqualTo("123456acdef"));
+        }
+    }
+}
