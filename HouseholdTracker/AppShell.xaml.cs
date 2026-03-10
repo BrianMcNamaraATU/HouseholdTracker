1ab68a3c9275a098409ad0e1d94802b5f0d31a3b
Brian McNamara <L00196743@atu.ie>
2026-03-10 00:00:21 +0000

    Add project files.

---
+++ HouseholdTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseholdTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HouseholdTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HouseholdTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
