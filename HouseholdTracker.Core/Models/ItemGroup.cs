af27c1e4455fa4b1c669d0170ad282535b964b00
Brian McNamara <L00196743@atu.ie>
2026-03-14 23:32:06 +0000

    ItemGroup class and tests added Closes #3 (#23)

---
+++ HouseholdTracker.Core/Models/ItemGroup.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+
+namespace HouseholdTracker.Core.Models;
+
+internal class ItemGroup
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DefaultIcon { get; set; } = 0;
+    public ItemSizes DefaultItemSize { get; set; } = ItemSizes.Grams;
+    public bool DefaultExpiryVisible { get; set; } = true;
+    public int DefaultLowPercentageWarning { get; set; } = 25;
+    public bool Enabled { get; set; } = true;
+    public bool OnlineMatched { get; set; } = true;
+    public int SortOrder { get; set; } = 0;
+
+    public ItemGroup() { }
+
+#pragma warning disable S107
+    public ItemGroup(int id, int userID, string name, int defaultIcon, ItemSizes defaultItemSize, bool defaultExpiryVisible, int defaultLowPercentageWarning, bool enabled, bool onlineMatched, int sortOrder)
+    {
+        Id = id;
+        UserId = userID;
+        Name = name;
+        DefaultIcon = defaultIcon;
+        DefaultItemSize = defaultItemSize;
+        DefaultExpiryVisible = defaultExpiryVisible;
+        DefaultLowPercentageWarning = defaultLowPercentageWarning;
+        Enabled = enabled;
+        OnlineMatched = onlineMatched;
+        SortOrder = sortOrder;
+    }
+#pragma warning restore
+}
+
+internal enum ItemSizes
+{
+    Grams = 1,
+    ML = 2
+};
