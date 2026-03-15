515a66c9e31e28393444d0166a855e8fdf82b505
Brian McNamara <L00196743@atu.ie>
2026-03-15 22:35:49 +0000

    Item model and tests added Closes #4 (#25)

---
+++ HouseholdTracker.Core/Models/Item.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+
+namespace HouseholdTracker.Core.Models;
+
+internal class Item
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Icon { get; set; } = 0;
+    public bool ExpiryVisible { get; set; } = true;
+    public int MaxSize { get; set; }
+    public ItemSizes ItemSize { get; set; } = ItemSizes.Grams;
+    /// <summary>
+    /// Displays the current level of an item. Avoids the use of decimals by declaring as int
+    /// </summary>
+    public int CurrentLevelAsPercentage { get; set; }
+    public int LowPercentageWarning { get; set; }
+    public DateOnly CurrentExpiry { get; set; } = DateOnly.MinValue;
+    public DateTime LastUpdateUTC { get; set; } = DateTime.MinValue;
+    public bool Enabled { get; set; } = true;
+    public int SortOrder { get; set; } = 0;
+
+    public Item() { }
+
+#pragma warning disable S107
+    public Item(int id, int userId, string name, int icon, bool expiryVisible, int maxSize, ItemSizes itemSize, int currentLevelAsPercentage, int lowPercentageWarning, DateOnly currentExpiry, DateTime lastUpdateUTC, bool enabled, int sortOrder)
+    {
+        Id = id;
+        UserId = userId;
+        Name = name;
+        Icon = icon;
+        ExpiryVisible = expiryVisible;
+        MaxSize = maxSize;
+        ItemSize = itemSize;
+        CurrentLevelAsPercentage = currentLevelAsPercentage;
+        LowPercentageWarning = lowPercentageWarning;
+        CurrentExpiry = currentExpiry;
+        LastUpdateUTC = lastUpdateUTC;
+        Enabled = enabled;
+        SortOrder = sortOrder;
+    }
+#pragma warning restore
+}
