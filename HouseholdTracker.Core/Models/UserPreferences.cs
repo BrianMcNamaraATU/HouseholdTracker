78ce41f48764560292c3571a706fbb1548417d54
Brian McNamara <L00196743@atu.ie>
2026-03-16 21:47:14 +0000

    UserPreferences class and tests added

---
+++ HouseholdTracker.Core/Models/UserPreferences.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+
+namespace HouseholdTracker.Core.Models;
+
+internal class UserPreferences
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public Preferences Preference { get; set; }
+    public bool Value { get; set; } = false;
+
+    public UserPreferences() { }
+
+    public UserPreferences(int id, int userId, Preferences preference, bool value)
+    {
+        Id = id;
+        UserId = userId;
+        Preference = preference;
+        Value = value;
+    }
+}
+
+internal enum Preferences
+{
+    None = 0,
+    Marketing = 1,
+    SharedAccountAcceptance = 2
+}
---
+++ HouseholdTracker.Tests/Models/UserPreferencesTests.cs
@@ -0,0 +1,44 @@
+﻿using HouseholdTracker.Core.Models;
+
+namespace HouseholdTracker.Tests.Models;
+
+/// <summary>
+/// Tests for the UserPreferences class
+/// </summary>
+[TestFixture]
+internal sealed class UserPreferencesTests
+{
+    /// <summary>
+    /// Ensure the expected default values are used in an empty constructor
+    /// </summary>
+    [Test]
+    public void DefaultConstructorTest()
+    {
+        var userPreferences = new UserPreferences();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(userPreferences.Id, Is.Zero);
+            Assert.That(userPreferences.UserId, Is.Zero);
+            Assert.That(userPreferences.Preference, Is.EqualTo(Preferences.None));
+            Assert.That(userPreferences.Value, Is.False);
+        }
+    }
+
+    /// <summary>
+    /// Ensure the values are set correctly in a non-empty constructor
+    /// </summary>
+    [Test]
+    public void DefaultSetterTest()
+    {
+        var userPreferences = new UserPreferences(1, 2, Preferences.Marketing, true);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(userPreferences.Id, Is.EqualTo(1));
+            Assert.That(userPreferences.UserId, Is.EqualTo(2));
+            Assert.That(userPreferences.Preference, Is.EqualTo(Preferences.Marketing));
+            Assert.That(userPreferences.Value, Is.True);
+        }
+    }
+}
