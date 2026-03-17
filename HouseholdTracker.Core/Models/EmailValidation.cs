19b5a6febddedd9674ff4d961de3cc9662b4548a
Brian McNamara <L00196743@atu.ie>
2026-03-17 11:48:35 +0000

    EmailValidation model and tests added (#33)

---
+++ HouseholdTracker.Core/Models/EmailValidation.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+
+namespace HouseholdTracker.Core.Models;
+
+internal class EmailValidation
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; private set; }
+    public int UserId { get; internal set; }
+    public string PreviousEmail { get; internal set; } = string.Empty;
+    public string NewEmail { get; internal set; } = string.Empty;
+    public string EmailCode { get; internal set; } = string.Empty;
+    public DateTime SentUTC { get; internal set; } = DateTime.MinValue;
+    public DateTime? ValidatedUTC { get; internal set; } = null;
+
+    public EmailValidation() { }
+
+    public EmailValidation(int id, int userId, string previousEmail, string newEmail, string emailCode, DateTime sentUTC, DateTime? validatedUTC)
+    {
+        Id = id;
+        UserId = userId;
+        PreviousEmail = previousEmail;
+        NewEmail = newEmail;
+        EmailCode = emailCode;
+        SentUTC = sentUTC;
+        ValidatedUTC = validatedUTC;
+    }
+}
---
+++ HouseholdTracker.Tests/Models/EmailValidationTests.cs
@@ -0,0 +1,50 @@
+﻿using HouseholdTracker.Core.Models;
+
+namespace HouseholdTracker.Tests.Models;
+
+/// <summary>
+/// Tests for the EmailValidation class
+/// </summary>
+[TestFixture]
+internal sealed class EmailValidationTests
+{
+    /// <summary>
+    /// Ensure the expected default values are used in an empty constructor
+    /// </summary>
+    [Test]
+    public void DefaultConstructorTest()
+    {
+        var emailValidation = new EmailValidation();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(emailValidation.Id, Is.Zero);
+            Assert.That(emailValidation.UserId, Is.Zero);
+            Assert.That(emailValidation.PreviousEmail, Is.EqualTo(string.Empty));
+            Assert.That(emailValidation.NewEmail, Is.EqualTo(string.Empty));
+            Assert.That(emailValidation.EmailCode, Is.EqualTo(string.Empty));
+            Assert.That(emailValidation.SentUTC, Is.EqualTo(DateTime.MinValue));
+            Assert.That(emailValidation.ValidatedUTC, Is.Null);
+        }
+    }
+
+    /// <summary>
+    /// Ensure the values are set correctly in a non-empty constructor
+    /// </summary>
+    [Test]
+    public void DefaultSetterTest()
+    {
+        var emailValidation = new EmailValidation(1, 2, "previous@email.com", "new@email.com", "ABC123", DateTime.MaxValue, DateTime.MinValue);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(emailValidation.Id, Is.EqualTo(1));
+            Assert.That(emailValidation.UserId, Is.EqualTo(2));
+            Assert.That(emailValidation.PreviousEmail, Is.EqualTo("previous@email.com"));
+            Assert.That(emailValidation.NewEmail, Is.EqualTo("new@email.com"));
+            Assert.That(emailValidation.EmailCode, Is.EqualTo("ABC123"));
+            Assert.That(emailValidation.SentUTC, Is.EqualTo(DateTime.MaxValue));
+            Assert.That(emailValidation.ValidatedUTC, Is.EqualTo(DateTime.MinValue));
+        }
+    }
+}
