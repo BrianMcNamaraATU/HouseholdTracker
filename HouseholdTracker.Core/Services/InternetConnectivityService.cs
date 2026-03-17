4838a0b06da1f0cf938227524deefdda15fd0590
Brian McNamara <L00196743@atu.ie>
2026-03-17 10:42:21 +0000

    InternetConnectivityService added and tests

---
+++ HouseholdTracker.Core/Services/InternetConnectivityService.cs
@@ -0,0 +1,50 @@
+﻿namespace HouseholdTracker.Core.Services;
+
+/// <summary>
+/// A class that provides checks for internet connectivity.
+/// As the CentralDatabase is currently stored locally we need this implementation
+/// for completion. It can be easily modified to provide actual results if the
+/// CentralDabase is moved online.
+/// It also allows forced simulation of true/false
+/// </summary>
+internal class InternetConnectivityService
+{
+    private readonly Func<bool> _connectivity;
+
+    private ConnectivityType _connectivityType = ConnectivityType.Default;
+
+    /// <summary>
+    /// Constructor for the class
+    /// </summary>
+    /// <param name="connectivityCheck">Uses Func-bool to check connectivity each time its accessed</param>
+    public InternetConnectivityService(Func<bool> connectivityCheck)
+    {
+        _connectivity = connectivityCheck;
+    }
+
+    internal bool HasInternetConnectivity
+    {
+        get
+        {
+            switch (_connectivityType)
+            {
+                case ConnectivityType.Connected: return true;
+                case ConnectivityType.Disconnected: return false;
+                default: return _connectivity();
+            }
+        }
+    }
+
+    internal void ForceConnection() => _connectivityType = ConnectivityType.Connected;
+
+    internal void ForceNoConnection() => _connectivityType = ConnectivityType.Disconnected;
+
+    internal void ResetToDefault() => _connectivityType = ConnectivityType.Default;
+
+    private enum ConnectivityType
+    {
+        Default = 0,
+        Connected = 1,
+        Disconnected = 2
+    }
+}
---
+++ HouseholdTracker.Tests/Services/InternerConnectivityServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using HouseholdTracker.Core.Services;
+
+namespace HouseholdTracker.Tests.Services;
+
+/// <summary>
+/// Tests for the InternetConnectivity Service
+/// </summary>
+public class InternerConnectivityServiceTests
+{
+    private InternetConnectivityService? _service;
+
+    /// <summary>
+    /// Test the results when actual internet connectivity exists
+    /// </summary>
+    [Test]
+    public async Task TestResults_WithActualInternetConnectivity()
+    {
+        _service = new InternetConnectivityService(() => true);
+        var result1 = _service.HasInternetConnectivity;
+        _service.ForceConnection();
+        var result2 = _service.HasInternetConnectivity;
+        _service.ForceNoConnection();
+        var result3 = _service.HasInternetConnectivity;
+        _service.ResetToDefault();
+        var result4 = _service.HasInternetConnectivity;
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result1, Is.True);
+            Assert.That(result2, Is.True);
+            Assert.That(result3, Is.False);
+            Assert.That(result4, Is.True);
+        }
+    }
+
+    /// <summary>
+    /// Test the results when no internet connectivity exists
+    /// </summary>
+    [Test]
+    public async Task TestResults_WithNoInternetConnectivity()
+    {
+        _service = new InternetConnectivityService(() => false);
+        var result1 = _service.HasInternetConnectivity;
+        _service.ForceConnection();
+        var result2 = _service.HasInternetConnectivity;
+        _service.ForceNoConnection();
+        var result3 = _service.HasInternetConnectivity;
+        _service.ResetToDefault();
+        var result4 = _service.HasInternetConnectivity;
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result1, Is.False);
+            Assert.That(result2, Is.True);
+            Assert.That(result3, Is.False);
+            Assert.That(result4, Is.False);
+        }
+    }
+}
